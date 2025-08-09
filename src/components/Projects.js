a59bd84a53101a41d3dd33833dc61be2ec467634
RossO <rossoliver3@gmail.com>
2025-08-09 02:18:16 +0100

    Update some project text.
    Added workflow files

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,6 +16,7 @@ function Projects() {
             For me the highlight is the ability for the app to generate pseudo random facts about the collection, including how many entries for a given artist,
             or how many additions were made in a random month/year.
           </p>
+          <p>I have deployed this application on my home network to a Raspberry Pi combined with a Touchscreen, allowing me to have a custom dashboard that I can interact with at any time.</p>
           <p>This project features a client written in <b>React</b> and a server written in <b>.NET 8</b> that uses the Discogs.com API to collect data.</p>
           <p>The application can also be deployed in  <b>Docker</b> and has it's own <b>Helm Charts</b></p>
         </div>
@@ -58,6 +59,7 @@ function Projects() {
         <p>The EA College Football game series contains an indepth skill tree for your created coach, in which there are many potential paths to take.
           I play in a competitive online league and discussion often drifts to tools or utilities that the players would like to use.
           So I thought I would take it upon myself to build one of them, a way to plan out how you intend to build your coach, allowing players to experiment and see what combinations of abilities they can or can't afford.
+          The game's own internal logic is coded into the tool, including dynamic pricing of abilities and unlock conditions.
         </p>
         <br></br>
         <p>I initially wrote this against the '25 version of the game, but came back to it following the '26 edition's release to fully flesh out the features.</p>

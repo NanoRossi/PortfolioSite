eaf37f58d2bf149146c380c8de9804a051b2ca3b
RossO <rossoliver3@gmail.com>
2026-08-29 23:49:44 +0100

    Update current job info

--- src/components/Entries/WorkHistory/SierraNevada.js
+++ src/components/Entries/WorkHistory/SierraNevada.js
@@ -5,9 +5,12 @@ export default function SierraNevada() {
         <div className="section">
             <ItemEntry
                 title="Senior Software Developer"
-                company="Sierra Nevada Corporation"
+                company="SNC Mission Systems UK"
                 dates="Jan 2026 - Present"
             >
+                <p>
+                    Due to confidentiality obligations, I am unable to share details of my current projects.
+                </p>
             </ItemEntry>
         </div>
     );
